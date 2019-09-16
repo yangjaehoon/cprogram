0dd795e64903c869db3314434829e6bc8e8e81cb
dlswns2480 <dlswns2480@naver.com>
2019-09-16 12:44:48 +0900

    ch2 labs added

--- sum.c
+++ sum.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main(void)
 {
-	int x;
-	int y;
-	int sum;
-	printf("First digit:");
-	scanf("%d",&x);
-	printf("Second digit:");
-	scanf("%d",&y);
-	sum=x+y;
-	printf("Sum:%d",sum);
+	int x,y,z,sum;
+
+	sum = 0;
+	printf("3개의 정수를 입력하세요(x,y,z):");
+	scanf("%d %d %d", &x,&y,&z);
+	sum +=x;
+	sum +=y;
+	sum +=z;
+	printf("3개 정수의 합은 %d\n",sum);
 	return 0;
 }
+
+
 
